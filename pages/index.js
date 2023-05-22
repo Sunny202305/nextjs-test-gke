fae9f00aa69ce38cd41a064234322a397468534b
Sunny <shengnan@flyntlok.com>
2023-05-22 10:22:28 -0400

    version 1

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Test cloud deploy promote version 1</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
