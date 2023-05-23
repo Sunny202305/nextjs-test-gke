cae9ef69a052e4aba9c820621e2e507d891ae2ad
Sunny <shengnan@flyntlok.com>
2023-05-23 09:34:12 -0400

    clean code

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 2</a> on Docker!
+          Welcome to <a href="https://nextjs.org">Next.js 3</a> on Docker!
         </h1>
 
         <p className={styles.description}>
